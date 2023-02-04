c90dc3a6ad6334beb7d75a834ad72242facb19af
SON7AE <bobsot0225@gmail.com>
2023-02-04 16:30:33 +0900

    익스프레스 - 미들웨어

--- express/app.js
+++ express/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 app.set('port', process.env.PORT || 3000)
 
+app.use((req, res) => {
+    console.log('모든 요청에 실행하고 싶어요.')
+    next()
+})
 app.get('/', (req, res) => {
+    // HTML 서빙
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+app.get('/about', (req, res) => {
     res.send('Opened Express Server')
+})
+
+app.post('/', (req, res) => {
+    res.send('Express POST')
 })
 
 app.listen(app.get('port'), () => {
